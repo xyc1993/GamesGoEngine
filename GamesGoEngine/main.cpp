7733d47a62b2356de6c884495973d39a1c19c8b2
mgrygo <m.grygo93@gmail.com>
2021-12-20 04:27:27 +0100

    Added an option to switch between wireframe & lit rendering

--- GamesGoEngine/main.cpp
+++ GamesGoEngine/main.cpp
@@ -85,8 +85,9 @@ void MainLoop(GLFWwindow* window)
 	GLfloat timeMultiplier = 1.0f;
 	currentTime = 0.0f;
 
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //use this for wireframe
-
+	bool wireframeOnly = false;
+	bool lastWireframeOnly = false;
+	
 	SceneExample_LitForward* activeScene = new SceneExample_LitForward();
 	
 	projection_global = glm::mat4(1.0f);
@@ -94,6 +95,19 @@ void MainLoop(GLFWwindow* window)
 		
 	while (!glfwWindowShouldClose(window))
 	{
+		if (wireframeOnly != lastWireframeOnly)
+		{
+			lastWireframeOnly = wireframeOnly;
+			if (wireframeOnly)
+			{
+				glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			}
+			else
+			{
+				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			}
+		}
+
 		GLfloat currentFrame = glfwGetTime();
 		deltaTime = timeMultiplier * (currentFrame - lastFrame);
 		lastFrame = currentFrame;
@@ -111,9 +125,9 @@ void MainLoop(GLFWwindow* window)
 
 		activeScene->Update();
 		
-		ImGui::Begin("Test ImGUI window");
-		ImGui::Text("ImGUI Text");
+		ImGui::Begin("Basic ImGUI window");
 		ImGui::SliderFloat("Time Scale", &timeMultiplier, 0.0f, 5.0f);
+		ImGui::Checkbox("Wireframe only", &wireframeOnly);
 		ImGui::End();
 
 		ImGui::Render();
