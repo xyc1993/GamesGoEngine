d4eadbb0fee21e149148b2632021a5f69959d602
mgrygo <m.grygo93@gmail.com>
2022-10-17 11:03:37 +0200

    Added more complex mesh to shadow mapping example scene

--- GamesGoEngine/SceneExample_DirectionalShadows.cpp
+++ GamesGoEngine/SceneExample_DirectionalShadows.cpp
@@ -1,5 +1,6 @@
 #include "SceneExample_DirectionalShadows.h"
 
+#include "MeshImported.h"
 #include "MeshPrimitiveCube.h"
 #include "MeshPrimitiveQuad.h"
 #include "MeshRenderer.h"
@@ -50,6 +51,42 @@ SceneExample_DirectionalShadows::SceneExample_DirectionalShadows()
 		cubeObject->GetTransform()->SetRotationEulerDegrees(cubeRotations[i]);
 		scene->AddGameObject(cubeObject);
 	}
+
+	// Add complex mesh
+	GameObject* nanoSuitObject = new GameObject();
+	nanoSuitObject->GetTransform()->SetPosition(glm::vec3(0.0f, -2.0f, 0.0f));
+	nanoSuitObject->GetTransform()->SetRotationEulerDegrees(glm::vec3(0.0f, 135.0f, 0.0f));
+	nanoSuitObject->GetTransform()->SetScale(glm::vec3(0.2f));
+	MeshRenderer* nanoSuitMeshRenderer = new MeshRenderer();
+	std::shared_ptr<MeshImported> nanoSuitMesh = std::make_shared<MeshImported>((GLchar*)"res/nanosuit/nanosuit.obj");
+	nanoSuitMeshRenderer->SetMesh(nanoSuitMesh);
+
+	const int NANOSUIT_MATERIALS_NUMBER = 7;
+	std::shared_ptr<Material> nanoSuitMaterials[NANOSUIT_MATERIALS_NUMBER];
+
+	std::vector<GLchar*> nanoSuitTexturePaths;
+	nanoSuitTexturePaths.push_back((GLchar*)"res/nanosuit/glass_dif.png");
+	nanoSuitTexturePaths.push_back((GLchar*)"res/nanosuit/leg_dif.png");
+	nanoSuitTexturePaths.push_back((GLchar*)"res/nanosuit/hand_dif.png");
+	nanoSuitTexturePaths.push_back((GLchar*)"res/nanosuit/glass_dif.png"); // this one is small 'lights' on the helmet under visor, no specific texture was given for those
+	nanoSuitTexturePaths.push_back((GLchar*)"res/nanosuit/arm_dif.png");
+	nanoSuitTexturePaths.push_back((GLchar*)"res/nanosuit/helmet_diff.png");
+	nanoSuitTexturePaths.push_back((GLchar*)"res/nanosuit/body_dif.png");
+
+	for (int i = 0; i < NANOSUIT_MATERIALS_NUMBER; i++)
+	{
+		nanoSuitMaterials[i] = std::make_shared<Material>("res/shaders/litShadowSimple.vert.glsl", "res/shaders/litShadowSimple.frag.glsl");
+		nanoSuitMaterials[i]->SetTextureByPath((GLchar*)"diffuseTexture", 0, nanoSuitTexturePaths[i]);
+		nanoSuitMaterials[i]->SetLightModel(LightModelType::LitForward);
+		nanoSuitMeshRenderer->SetMaterial(nanoSuitMaterials[i], i);
+	}
+
+	nanoSuitObject->AddComponent(nanoSuitMeshRenderer);
+
+	std::string nanoSuitName = "nanosuit";
+	nanoSuitObject->SetName(nanoSuitName);
+
+	scene->AddGameObject(nanoSuitObject);
 
 	// Add source of light
 	GameObject* directionalLightObject = new GameObject();
