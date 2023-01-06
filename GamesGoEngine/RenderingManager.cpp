c11255e6576992c70e8341b7399d433093e131de
mgrygo <m.grygo93@gmail.com>
2023-01-06 03:18:10 +0100

    Specular value should not be white by default anymore

--- GamesGoEngine/RenderingManager.cpp
+++ GamesGoEngine/RenderingManager.cpp
@@ -531,7 +531,7 @@ void RenderingManager::UpdateGBuffer()
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
 
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glm::mat4 model = glm::mat4(1.0f);
