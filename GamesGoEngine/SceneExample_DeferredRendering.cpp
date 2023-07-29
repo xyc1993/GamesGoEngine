9d07ba3d639095c8fd59e0330c6577dfb082597b
xyc1993 <m.grygo93@gmail.com>
2023-07-29 18:56:01 +0200

    Updated deferred rendering scene & added extra textures

--- GamesGoEngine/SceneExample_DeferredRendering.cpp
+++ GamesGoEngine/SceneExample_DeferredRendering.cpp
@@ -1,13 +1,13 @@
 #include "SceneExample_DeferredRendering.h"
 
-#include "MeshPrimitiveCube.h"
-#include "MeshPrimitiveSphere.h"
+#include "MeshPrimitivesPool.h"
 #include "MeshRenderer.h"
 
 SceneExample_DeferredRendering::SceneExample_DeferredRendering()
 {
-	std::shared_ptr<MeshPrimitiveCube> cubeMesh = std::make_shared<MeshPrimitiveCube>();
-	std::shared_ptr<MeshPrimitiveSphere> sphereMesh = std::make_shared<MeshPrimitiveSphere>();
+	std::shared_ptr<MeshPrimitiveCube> cubeMesh = MeshPrimitivesPool::GetCubePrimitive();
+	std::shared_ptr<MeshPrimitiveQuad> quadMesh = MeshPrimitivesPool::GetQuadPrimitive();
+	std::shared_ptr<MeshPrimitiveSphere> sphereMesh = MeshPrimitivesPool::GetSpherePrimitive();
 
 	std::shared_ptr<Material> boxDeferredMaterial = std::make_shared<Material>("res/shaders/RenderPipeline/gBuffer.vert.glsl", "res/shaders/RenderPipeline/gBuffer.frag.glsl");
 	boxDeferredMaterial->SetTextureByPath((GLchar*)"texture_diffuse1", 0, (GLchar*)"res/box/container2_diffuse.png");
@@ -15,14 +15,20 @@ SceneExample_DeferredRendering::SceneExample_DeferredRendering()
 	boxDeferredMaterial->SetFloat("enableLight", 1.0f);
 	boxDeferredMaterial->SetLightModel(LightModelType::LitDeferred);
 
+	std::shared_ptr<Material> bricksDeferredMaterial = std::make_shared<Material>("res/shaders/RenderPipeline/gBuffer.vert.glsl", "res/shaders/RenderPipeline/gBuffer.frag.glsl");
+	bricksDeferredMaterial->SetTextureByPath((GLchar*)"texture_diffuse1", 0, (GLchar*)"res/textures/brickDiffNormSpec/brick_wall2-diff-1024.tga");
+	bricksDeferredMaterial->SetTextureByPath((GLchar*)"texture_specular1", 1, (GLchar*)"res/textures/brickDiffNormSpec/brick_wall2-spec-1024.tga");
+	bricksDeferredMaterial->SetFloat("enableLight", 1.0f);
+	bricksDeferredMaterial->SetLightModel(LightModelType::LitDeferred);
+
 	std::vector<glm::vec3> boxPositions;
-	boxPositions.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
-	boxPositions.push_back(glm::vec3(0.0f, -2.0f, 0.0f));
-	boxPositions.push_back(glm::vec3(0.0f, 6.0f, 0.0f));
-	boxPositions.push_back(glm::vec3(-7.0f, 0.0f, 0.0f));
-	boxPositions.push_back(glm::vec3(7.0f, 0.0f, 0.0f));
-	boxPositions.push_back(glm::vec3(0.0f, 0.0f, -7.0f));
-	boxPositions.push_back(glm::vec3(0.0f, 0.0f, 7.0f));
+	boxPositions.push_back(glm::vec3(0.0f, 1.5f, 0.0f));
+	boxPositions.push_back(glm::vec3(0.0f, -1.0f, 0.0f));
+	boxPositions.push_back(glm::vec3(0.0f, 4.0f, 0.0f));
+	boxPositions.push_back(glm::vec3(-5.0f, 1.5f, 0.0f));
+	boxPositions.push_back(glm::vec3(5.0f, 1.5f, 0.0f));
+	boxPositions.push_back(glm::vec3(0.0f, 1.5f, -5.0f));
+	boxPositions.push_back(glm::vec3(0.0f, 1.5f, 5.0f));
 
 	for (size_t i = 0; i < boxPositions.size(); i++)
 	{
@@ -41,14 +47,58 @@ SceneExample_DeferredRendering::SceneExample_DeferredRendering()
 		scene->AddGameObject(boxObject);
 	}
 
+	// Add walls, floor and ceiling
+	std::vector<glm::vec3> boundsPositions;
+	boundsPositions.push_back(glm::vec3(0.0f, -2.0f, 0.0f));
+	boundsPositions.push_back(glm::vec3(0.0f, 6.0f, 0.0f));
+	boundsPositions.push_back(glm::vec3(-7.0f, 2.0f, 0.0f));
+	boundsPositions.push_back(glm::vec3(7.0f, 2.0f, 0.0f));
+	boundsPositions.push_back(glm::vec3(0.0f, 2.0f, -7.0f));
+	boundsPositions.push_back(glm::vec3(0.0f, 2.0f, 7.0f));
+
+	std::vector<glm::vec3> boundsRotations;
+	boundsRotations.push_back(glm::vec3(-90.0f, 0.0f, 0.0f));
+	boundsRotations.push_back(glm::vec3(90.0f, 0.0f, 0.0f));
+	boundsRotations.push_back(glm::vec3(0.0f, 90.0f, 0.0f));
+	boundsRotations.push_back(glm::vec3(0.0f, -90.0f, 0.0f));
+	boundsRotations.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
+	boundsRotations.push_back(glm::vec3(0.0f, 180.0f, 0.0f));
+
+	for (size_t i = 0; i < boundsPositions.size(); i++)
+	{
+		GameObject* boundObject = new GameObject();
+		MeshRenderer* boundMeshRenderer = new MeshRenderer();
+		boundMeshRenderer->SetMesh(quadMesh);
+
+		std::string name = "bound_";
+
+		boundMeshRenderer->SetMaterial(bricksDeferredMaterial);
+		boundObject->AddComponent(boundMeshRenderer);
+		boundObject->GetTransform()->SetPosition(boundsPositions[i]);
+		boundObject->GetTransform()->SetRotationEulerDegrees(boundsRotations[i]);
+		if (i <= 1) // floor & ceiling
+		{
+			boundObject->GetTransform()->SetScale(glm::vec3(7.0f));
+		}
+		else //rest of the walls
+		{
+			boundObject->GetTransform()->SetScale(glm::vec3(7.0f, 4.0f, 7.0f));
+		}
+
+		name.append(std::to_string(i));
+		boundObject->SetName(name);
+
+		scene->AddGameObject(boundObject);
+	}
+
 	// Add sources of light
 	constexpr int lightsNumber = 4;
 
 	std::vector<glm::vec3> lightPositions;
-	lightPositions.push_back(glm::vec3(0.9f, 0.0f, 0.9f));
-	lightPositions.push_back(glm::vec3(0.0f, -1.0f, 0.0f));
-	lightPositions.push_back(glm::vec3(-1.2f, 1.5f, -0.3f));
-	lightPositions.push_back(glm::vec3(-0.8f, -0.8f, 0.4f));
+	lightPositions.push_back(glm::vec3(0.9f, 1.5f, 0.9f));
+	lightPositions.push_back(glm::vec3(0.0f, 0.5f, 0.0f));
+	lightPositions.push_back(glm::vec3(-1.2f, 3.0f, -0.3f));
+	lightPositions.push_back(glm::vec3(-0.8f, 0.7f, 0.4f));
 
 	std::vector<glm::vec3> lightColors;
 	lightColors.push_back(glm::vec3(9.0f, 0.0f, 0.0f));
