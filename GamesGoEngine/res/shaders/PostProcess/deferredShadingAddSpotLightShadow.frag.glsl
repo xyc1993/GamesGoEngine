34ebb9e9b610ac3cd6b41379f46e38a54417c3fb
mgrygo <m.grygo93@gmail.com>
2023-08-17 08:25:31 +0200

    Implemented PBR for deferred spot light

--- GamesGoEngine/res/shaders/PostProcess/deferredShadingAddSpotLightShadow.frag.glsl
+++ GamesGoEngine/res/shaders/PostProcess/deferredShadingAddSpotLightShadow.frag.glsl
@@ -9,8 +9,10 @@ layout(binding = 1) uniform sampler2D spotLightShadowMap;
 layout(binding = 2) uniform sampler2D gPosition;
 layout(binding = 3) uniform sampler2D gNormal;
 layout(binding = 4) uniform sampler2D gAlbedo;
-layout(binding = 5) uniform sampler2D gSpecular;
-layout(binding = 6) uniform sampler2D gEmissive;
+layout(binding = 5) uniform sampler2D gMetallic;
+layout(binding = 6) uniform sampler2D gRoughness;
+layout(binding = 7) uniform sampler2D gAmbientOcclusion;
+layout(binding = 8) uniform sampler2D gEmissive;
 
 uniform mat4 spotLightSpaceMatrix;
 
@@ -73,51 +75,114 @@ float ShadowCalculation(sampler2D sampledTexture, vec4 fragPosLightSpace, float 
     return shadow;
 }
 
+const float PI = 3.14159265359;
+
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a = roughness*roughness;
+    float a2 = a*a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
+
+    float nom   = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
+
+    float nom   = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+
 void main()
-{             
-    // retrieve existing color in case current pixel was already lit in the previous pass
-    vec3 screenColor = texture(screenTexture, TexCoords).rgb;;
-    // retrieve data from gbuffer
-    vec3 fragPos = texture(gPosition, TexCoords).rgb;
-    vec3 normal = texture(gNormal, TexCoords).rgb;
-    vec3 color = texture(gAlbedo, TexCoords).rgb;
-    float specularTex = texture(gSpecular, TexCoords).r;
+{
+    vec3 worldPos = texture(gPosition, TexCoords).rgb;
+    vec3 albedo = pow(texture(gAlbedo, TexCoords).rgb, vec3(2.2));
+    float metallic = texture(gMetallic, TexCoords).r;
+    float roughness = texture(gRoughness, TexCoords).r;    
     vec3 emissive = texture(gEmissive, TexCoords).rgb;
-    
-    vec3 finalColor = screenColor;
-    // then calculate lighting as usual
-    vec3 lighting  = vec3(0.0);
-    vec3 viewDir  = normalize(cameraPos - fragPos);
 
-    // diffuse
-    vec3 lightDir = normalize(spotLight.position - fragPos);
-	float diff = max(dot(normal, lightDir), 0.0);
+    vec3 N = texture(gNormal, TexCoords).rgb;
+    vec3 V = normalize(cameraPos - worldPos);
 
-    // specular
-    vec3 halfwayDir = normalize(lightDir + viewDir);
-	float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
-	    
-	//spotlight
-	float theta = dot(lightDir, normalize(-spotLight.direction));
+    // calculate reflectance at normal incidence; if dia-electric (like plastic) use F0 
+    // of 0.04 and if it's a metal, use the albedo color as F0 (metallic workflow)    
+    vec3 F0 = vec3(0.04); 
+    F0 = mix(F0, albedo, metallic);
+
+    // reflectance equation
+    vec3 Lo = vec3(0.0);
+    // calculate per-light radiance
+    vec3 L = normalize(-spotLight.direction);
+    vec3 H = normalize(V + L);
+    // point light specular is used since it's brightest, relic from older light model
+    vec3 lightDir = normalize(spotLight.position - worldPos);
+    float theta = dot(lightDir, normalize(-spotLight.direction));
 	float epsilon = spotLight.cutOff - spotLight.outerCutOff;
 	float intensity = clamp((theta - spotLight.outerCutOff) / epsilon, 0.0, 1.0);
+    vec3 radiance = intensity * spotLight.specular;
 
-	vec3 diffuse = spotLight.diffuse * diff * color;
-	vec3 specular = spotLight.specular * spec * specularTex;
+    // Cook-Torrance BRDF
+    float NDF = DistributionGGX(N, H, roughness);   
+    float G = GeometrySmith(N, V, L, roughness);      
+    vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
+           
+    vec3 numerator = NDF * G * F; 
+    float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001; // + 0.0001 to prevent divide by zero
+    vec3 specular = numerator / denominator;
+        
+    // kS is equal to Fresnel
+    vec3 kS = F;
+    // for energy conservation, the diffuse and specular light can't
+    // be above 1.0 (unless the surface emits light); to preserve this
+    // relationship the diffuse component (kD) should equal 1.0 - kS.
+    vec3 kD = vec3(1.0) - kS;
+    // multiply kD by the inverse metalness such that only non-metals 
+    // have diffuse lighting, or a linear blend if partly metal (pure metals
+    // have no diffuse light).
+    kD *= 1.0 - metallic;	  
 
-    float distance = length(spotLight.position - fragPos);
-    float attenuation = 1.0 / (spotLight.constant + spotLight.linear * distance + spotLight.quadratic * distance * distance);
-	lighting = attenuation * intensity * (diffuse + specular);
+    // scale light by NdotL
+    float NdotL = max(dot(N, L), 0.0);        
 
-    // shadows
-    vec4 fragPosSpotLightSpace = spotLightSpaceMatrix * vec4(fragPos, 1.0);
+    // add to outgoing radiance Lo
+    Lo += (kD * albedo / PI + specular) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
+
+    vec3 lighting = Lo;
+
+    // calculate and apply shadow
+    vec4 fragPosSpotLightSpace = spotLightSpaceMatrix * vec4(worldPos, 1.0);
     float shadow = ShadowCalculation(spotLightShadowMap, fragPosSpotLightSpace, 0.004);
     lighting = (1.0f - shadow) * lighting;
 
-    finalColor += lighting;
+    // emissive
+    vec3 color = lighting + emissive;
 
-     // emissive
-    finalColor += emissive;
+    // add screen texture in case we're additively combining light
+    color += texture(screenTexture, TexCoords).rgb;
 
-    FragColor = vec4(finalColor, 1.0);
+    FragColor = vec4(color, 1.0);
 }
