1e53dde228cfa82201626ace71990a376576ec9f
mgrygo <m.grygo93@gmail.com>
2023-09-01 22:18:59 +0200

    Now game objects can be deselected

--- GamesGoEngine/InputEditorUtilities.cpp
+++ GamesGoEngine/InputEditorUtilities.cpp
@@ -16,19 +16,19 @@ namespace GamesGoEngine
 
 		if (Scene* activeScene = SceneManager::GetActiveScene())
 		{
-			unsigned int objectId = RenderingManager::GetObjectIdAt(x, y);
-			
-			GameObject* selectedGameObject = activeScene->GetGameObjectWithId(objectId);
+			// If there was other game object that was selected, deselect it
+			// We want to always deselect the current object in order to have an option to deselect items by pressing on the void space
+			GameObject* previouslySelectedGameObject = activeScene->GetSelectedGameObject();
+			if (previouslySelectedGameObject != nullptr)
+			{
+				previouslySelectedGameObject->SetSelected(false);
+			}
 
+			// Get the id and try to find a matching game object, select it if found
+			const int objectId = RenderingManager::GetObjectIdAt(x, y);			
+			GameObject* selectedGameObject = activeScene->GetGameObjectWithId(objectId);
 			if (selectedGameObject != nullptr)
 			{
-				// if there was other game object that was selected, deselect it
-				GameObject* previouslySelectedGameObject = activeScene->GetSelectedGameObject();
-				if (previouslySelectedGameObject != nullptr)
-				{
-					previouslySelectedGameObject->SetSelected(false);
-				}
-
 				selectedGameObject->SetSelected(true);
 			}
 		}
