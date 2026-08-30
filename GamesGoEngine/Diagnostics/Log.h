d99d5e6cf5a34af73c241419b2d4e7fae730f14a
mgrygo <m.grygo93@gmail.com>
2026-08-30 19:47:04 +0200

    Prepared project for linking dll

--- GamesGoEngine/Diagnostics/Log.h
+++ GamesGoEngine/Diagnostics/Log.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#ifdef GAMES_GO_ENGINE_EXPORTS
+#define GAMES_GO_ENGINE_API __declspec(dllexport)
+#else
+#define GAMES_GO_ENGINE_API __declspec(dllimport)
+#endif
+
 namespace GamesGoEngine
 {
-	class Log
+	class GAMES_GO_ENGINE_API Log
 	{
 	public:
 		static void Info(const char* message);
